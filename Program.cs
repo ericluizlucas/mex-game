cff851685b3af2ed3b006f18de08560998301e4f
eric <eric@2upsolucoes.com.br>
2023-03-23 16:51:02 -0300

    Algoritmo para resolucao do problema proposto MEX Game

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿class Solver
+{
+
+    static int Solve(int[] arr)
+    {
+        int mex = -1;
+        for (int i = 0; i <= arr.Length; i++)
+        {
+            bool exists = false;
+            foreach (var item in arr)
+            {
+                if (item == i)
+                    exists = true;
+            }
+            if (!exists) return mex = i;
+        }
+        return mex;
+    }
+
+    static void Main()
+    {
+        int[] arr = new int[] { 1, 0, 2, 5 };
+        int mex = Solve(arr);
+
+        Console.WriteLine(mex);
+
+    }
+}
+
+
+
+
---
+++ README.md
@@ -0,0 +1,3 @@
+O MEX de um array é definido como o exemplo mínimo não-negativo não presente no array. Por exemplo, o MEX de [1,0,3,2,5] é 4 e o MEX de [1,2] é o 0.
+
+Dado um array, retorne o MEX deste array.
